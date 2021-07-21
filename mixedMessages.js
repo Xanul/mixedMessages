c6b6a6028ebf19b3ef66bbe3a8cb3bf4eaf0e94f
Rodrigo Rivas <rivas.060@gmail.com>
2021-07-21 11:55:10 -0500

    Created the function to create the randon name using random number generator

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,52 +1,62 @@
-let first = [
-  "Adelard",
-  "Bungo",
-  "Seredic",
-  "Mosco",
-  "Ferdinand",
-  "Fortinbras",
-  "Malva",
-  "Pearl",
-  "Drogo",
-  "Adalgrim",
-  "Rose",
-  "Belladona",
-  "Estella",
-  "Celandine",
-  "Hildigrim",
-  "Nob",
-  "Salvia",
-  "Rudigar",
-  "Wilcome",
-  "Pervina",
-  "Balbo",
-  "Falco",
-  "Hob",
-  "Otho",
-  "Togo",
-  "Myrtle",
-];
-let middle = [
-  "Bracegirdle",
-  "Burrows",
-  "Proudfoot",
-  "Roper",
-  "Greenhand",
-  "Gardner",
-  "Took",
-  "Smallburrow",
-  "Fairbairn",
-  "Cotton",
-];
-let end = [
-  "Bree",
-  "Hobbiton",
-  "Hardbottle",
-  "Crickhollow",
-  "Frogmorton",
-  "Bucklebury",
-  "Woodhal",
-  "Tuckborough",
-  "Bywater",
-  "Michel Delving",
-];
+function generateHobbitName() {
+  let first = [
+    "Adelard",
+    "Bungo",
+    "Seredic",
+    "Mosco",
+    "Ferdinand",
+    "Fortinbras",
+    "Malva",
+    "Pearl",
+    "Drogo",
+    "Adalgrim",
+    "Rose",
+    "Belladona",
+    "Estella",
+    "Celandine",
+    "Hildigrim",
+    "Nob",
+    "Salvia",
+    "Rudigar",
+    "Wilcome",
+    "Pervina",
+    "Balbo",
+    "Falco",
+    "Hob",
+    "Otho",
+    "Togo",
+    "Myrtle",
+  ];
+  let middle = [
+    "Bracegirdle",
+    "Burrows",
+    "Proudfoot",
+    "Roper",
+    "Greenhand",
+    "Gardner",
+    "Took",
+    "Smallburrow",
+    "Fairbairn",
+    "Cotton",
+  ];
+  let end = [
+    "Bree",
+    "Hobbiton",
+    "Hardbottle",
+    "Crickhollow",
+    "Frogmorton",
+    "Bucklebury",
+    "Woodhal",
+    "Tuckborough",
+    "Bywater",
+    "Michel Delving",
+  ];
+
+  let firstIndex = Math.floor(Math.random() * first.length);
+  let middleIndex = Math.floor(Math.random() * middle.length);
+  let endIndex = Math.floor(Math.random() * end.length);
+
+  console.log(
+    `Your random Hobbit name is ${first[firstIndex]} ${middle[middleIndex]} ${end[endIndex]}`
+  );
+}
